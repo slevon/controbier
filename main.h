ff9fdbe498d66f2303e46ab3118ace2688db99b5
slevon <r.raekow@gmail.com>
2015-06-18 15:22:51 +0200

    Updated STEP
    STEP struct to have a init state and  a heat and minute state to be hold.

--- main.h
+++ main.h
@@ -3,8 +3,9 @@
 #include <Arduino.h>
 
 typedef struct STEP{
-  enum TYPE{WAIT,HEAT,END} type;
-  byte value; //min or temperatue
+  enum TYPE{INIT,WAIT,HEAT,END} type;  //Init->Targettemp for startup , WAIT -> hold given temp for given minute, HEAT -> heat to temp , END -> recipe is in ended state
+  byte temp; //temperatue for PID
+  byte minute; // only applicable for WAIT type (holds temp for minute minutes)
 };
 
 #define MAX_NUM_OF_STEPS 25
